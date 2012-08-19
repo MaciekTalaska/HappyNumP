c0e0e259ddbbf34a502b10b6e6632fc7637a7c8f
MaciekTalaska <maciek.talaska@gmail.com>
2012-08-19 17:32:41 +0200

    NEW: new method of computation (slow)

--- HappyNumbers.py
+++ HappyNumbers.py
@@ -3,6 +3,9 @@ import math
 import sys
 import getopt
 
+allhappy = list()
+allsad = list()
+
 def main(argv=None):
     if argv is None:
 	argv = sys.argv
@@ -11,12 +14,16 @@ def main(argv=None):
 	sys.exit(-1)
     start = int(argv[1])
     stop = int(argv[2])
-    for number in range(start, stop+1):
-	allnumbers = list()
-	n = is_happy(number, allnumbers)
-	print(str(number) + " is" + (" " if n else " NOT ") + "happy")
-	#print_sequence(allnumbers) 
-	#print(allnumbers)
+    if len(argv) > 3 and argv[3] == 's':
+        for number in range(start, stop+1):
+	    allnumbers = list()
+	    n = is_happy(number, allnumbers)
+	    print( "%s %s" % (number, n))
+    else:
+        for number in range(start, stop+1):
+	    allnumbers = list()
+	    n = is_happy_quick(number, allnumbers)
+	    print("%s %s" % (number, n))
 
 def is_happy(number, allnumbers):
     if number in allnumbers:
@@ -33,6 +40,44 @@ def is_happy(number, allnumbers):
 	partial = int(pow(int(c), 2))
         newnumber += partial
     return is_happy(newnumber, allnumbers)
+
+def is_happy_quick( number, allnumbers):
+    global allsad
+    global allhappy
+    if number in allsad:
+	populate_sad(allnumbers)
+	return False
+    if number in allnumbers:
+	return False
+    if number in allhappy:
+	return True
+    if number == 1:
+	populate_happy(allnumbers)
+	return True
+    if number == 0:
+	return False
+    allnumbers.append(number)
+    characters = str(number)
+    newnumber = 0
+    for c in characters:
+	partial = int(pow(int(c),2))
+	newnumber += partial
+    return is_happy_quick(newnumber, allnumbers)
+
+def populate_sad( sequence ):
+    global allsad
+    populate(allsad, sequence)
+    
+def populate_happy( sequence):
+    global allsad
+    populate(allhappy, sequence)
+    
+def populate( destination, source):
+    for num in source:
+	if num not in destination:
+	    destination.append(num)
+    destination.sort()
+    
 
 def print_sequence( sequence ):
     for i in range(len(sequence)):
